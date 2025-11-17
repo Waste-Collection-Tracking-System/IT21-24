55d929739af95f151f7825fb836c0ca5207ab19b
luckrew0123 <mangkabongbenchpiet@gmail.com>
2025-11-17 17:02:27 +0800

    Added notify when canceled and put map on different wepage

---
+++ maps.js
@@ -0,0 +1,27 @@
+// Get query parameter
+const urlParams = new URLSearchParams(window.location.search);
+const place = urlParams.get('place') || 'Tankulan';
+
+// Initialize Map
+function initMap() {
+  const map = L.map('map').setView([8.360053, 124.868342], 17);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; OpenStreetMap'
+  }).addTo(map);
+
+  fetch('maps.json')
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(pin => {
+        if(pin.barangay === place){ // Show only selected barangay
+          const marker = L.marker([pin.lat, pin.lng]).addTo(map);
+          marker.bindPopup(`<b>${pin.title}</b><br>${pin.description}`);
+        }
+      });
+    })
+    .catch(err => console.error(err));
+}
+
+document.addEventListener("DOMContentLoaded", initMap);
---
+++ mapsData.js
@@ -0,0 +1,16 @@
+export const mapPins = [
+    {
+        "lat": 8.36005,
+        "lng": 124.86834,
+        "img": "images/pin1.jpg",
+        "title": "Location 1",
+        "description": "Description here"
+    },
+    {
+        "lat": 8.36090,
+        "lng": 124.86910,
+        "img": "images/pin2.jpg",
+        "title": "Location 2",
+        "description": "Another description"
+    }
+];
